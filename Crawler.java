0198d9d37b1e6eec0408b5cb75b13b502662715b
ngominhtu <76554470+becacabe2002@users.noreply.github.com>
2022-11-27 18:03:21 +0700

    Adding comments on Crawler class

--- Crawler.java
+++ Crawler.java
@@ -19,27 +19,43 @@ public class Crawler{
      */
     public static void crawl(int level, String url, ArrayList<String> visited) {
         if(level <=5){
+        	/**
+        	 * Document extends Element
+        	 * a HTML Document
+        	 */
             Document doc = request(url, visited);
             
             if (doc != null){
-                for (Element link: doc.select("a[href]")){
-                    String next_link = link.absUrl("href");
+            	// Element 	- consists of HTML elements
+                for (Element link: doc.select("a[href]")){//find tag <a> with href attr in the doc
+                    String next_link = link.absUrl("href");// get an absolute URL from a[href]
                     if (visited.contains(next_link)== false){
-                        crawl(level++, next_link, visited);
+                        crawl(level++, next_link, visited);// crawler follows the link to a deeper level
                     }
                 }
             }
         }
     }
+    /**
+     * 
+     * @param url
+     * @param v
+     * @return Document that have
+     */
     private static Document request(String url, ArrayList<String> v){
         try {
-            Connection con = Jsoup.connect(url);
-            Document doc = con.get();
+        	/**
+        	 * Connection inferface - a HTTP client and session obj
+        	 * to fetch content from the web
+        	 * and parse them into Document
+        	 */
+            Connection con = Jsoup.connect(url);// create a new connection
+            Document doc = con.get();// execute GET, the parse the result
 
-            if(con.response().statusCode() == 200){
+            if(con.response().statusCode() == 200){ // status code for successful request
                 System.out.println("Link:" + url);
                 System.out.println(doc.title());
-                v.add(url);
+                v.add(url);// add url to ArrayList
                 return doc;
             }
             return null;
